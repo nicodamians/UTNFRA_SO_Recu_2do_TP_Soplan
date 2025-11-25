bd588d33d9610d333ebd120b58f83f960d682cb3
nicodamians <nicolasdamian1989@gmail.com>
2025-11-25 19:16:23 +0000

    Resolucion Punto_D: Ansible

---
+++ RTA_Examen_20251124/Punto_D.sh
@@ -0,0 +1,12 @@
+# Resolucion Punto_C Ansible
+
+# mkdir -p roles/{Crea_Carpetas_nsoplan,Cambia_Propiedad_nsoplan,Sudoers_nsoplan}/tasks
+# touch touch roles/Crea_Carpetas_nsoplan/tasks/main.yml
+# touch roles/Cambia_Propiedad_nsoplan/tasks/main.yml
+# touch roles/Sudoers_nsoplan/tasks/main.yml
+# sudo chown nsoplan:nsoplan playbook.yml
+# chmod u+w playbook.yml
+# nano playbook.yml
+# ansible-playbook playbook.yml
+
+
