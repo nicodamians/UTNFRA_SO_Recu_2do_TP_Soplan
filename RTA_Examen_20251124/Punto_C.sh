50210a42c0ca7ba56938f45e92d38dc4a6ae57b1
nicodamians <nicolasdamian1989@gmail.com>
2025-11-25 18:11:50 +0000

    Resolucion Punto_C: imagen en Docker

---
+++ RTA_Examen_20251124/Punto_C.sh
@@ -0,0 +1,13 @@
+# Comandos Punto C - Docker
+
+# id -u nsoplan
+# sudo grep "^nsoplan:" /etc/shadow
+# nano web/file/info.txt
+# sudo docker build -t web2-soplan
+# sudo docker tag web2-soplan:latest nicodamians/web2-soplan:latest
+# sudo docker login -u nicodamians
+# sudo docker push nicodamians/web2-soplan:latest
+# nano docker-compose.yml
+# sudo docker compose up -d
+# sudo docker ps
+
